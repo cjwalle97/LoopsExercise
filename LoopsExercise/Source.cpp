ed810ddc738935db29a380008779bcbd6747e8d9
cjwalle97 <cjwalle97@gmail.com>
2017-01-06 03:13:49 -0600

    Update
    Update

--- LoopsExercise/Source.cpp
+++ LoopsExercise/Source.cpp
@@ -3,19 +3,19 @@
 int main()
 {
 	//1. make a loop to iterate backwards from 100-0
-	for (int i = 100; i != 0; i--)
+	for (int i = 100; i >= 0; i--)
 	{
 		std::cout << i << std::endl;
 	}
-	for (int i = 100; i != -1; i--)
+	for (int i = 99; i >= 0; i--)
 	{
 		std::cout << i << std::endl;
 	}
-	for (int i = 101; i != 0; i--)
+	for (int i = 100; i > 0; i--)
 	{
 		std::cout << i << std::endl;
 	}
-	for (int i = 100; i != 0; i -= 2)
+	for (int i = 100; i >= 0; i -= 2)
 	{
 		std::cout << i << std::endl;
 	}
@@ -23,48 +23,91 @@ int main()
 	//2. iterate through 0 - 100 replace 3 with Fizz and 5 with Buzz
 	for (int k = 0; k < 100; k++)
 	{
-		if(k % 3 = 0)
+		if (k % 3 == 0 & k % 5 == 0)
 		{
-			std::cout << "Fizz";
+			std::cout << "FizzBuzz" << std::endl;
 		}
-		if (k % 5 = 0)
+		if (k % 3 == 0)
 		{
-			std::cout << "Buzz";
+			std::cout << "Fizz" << std::endl;
+			continue;
+		}
+		if (k % 5 == 0)
+		{
+			std::cout << "Buzz" << std::endl;
+			continue;
 		}
 		else
 		{
-			std::cout << k;
+			std::cout << k << std::endl;
 		}
 	}
 	//3.
 	for (int i = 0; i < 1000; i++)
 	{
 		int count = 0;
-		if (i % 3 = 0 || i % 5 = 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			count += i;
 		}
-		return count;
 	}
-	
+
 	//4.
-	int othercount = 0;
-	int i = 1000;
-	while (i>0)
+	int i = 100;
+	while (i >= 0)
 	{
 		i -= 1;
-		if(i % 3 = 0)
+		std::cout << i << std::endl;
+	}
+	int i = 99;
+	while (i >= 0)
+	{
+		i -= 1;
+		std::cout << i << std::endl;
+	}
+	int i = 100;
+	while (i > 0)
+	{
+		i -= 1;
+		std::cout << i << std::endl;
+	}
+	int i = 100;
+	while (i >= 0)
+	{
+		i -= 2;
+		std::cout << i << std::endl;
+	}
+
+	int k = 0;
+	while (k < 100)
+	{
+		if (k % 3 == 0 & k % 5 == 0)
 		{
-			
-			std::cout << i;
+			std::cout << "FizzBuzz" << std::endl;
 		}
-		if (i % 5 = 0)
+		if (k % 3 == 0)
 		{
-			std::cout << i;
+			std::cout << "Fizz" << std::endl;
+			continue;
 		}
-		if (i % 3 = 0 || i % 5 = 0)
+		if (k % 5 == 0)
 		{
-			othercount += i;
+			std::cout << "Buzz" << std::endl;
+			continue;
+		}
+		else
+		{
+			std::cout << k << std::endl;
+		}
+	}
+
+	int j = 0;
+	int othercount = 0;
+	while (j <= 1000)
+	{
+		if (j % 3 == 0 || j % 5 == 0)
+		{
+			othercount += j;
 		}
 	}
 }
